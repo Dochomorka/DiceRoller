dbd6d51290cd740d8f728ad741ba976df2918c86
Yimesgen Morka <webDesigner299@gmail.com>
2018-12-03 19:07:52 +0100

    Finished App after correcting the bug in rollDice() function

--- app/src/main/java/tech/cursor/com/diceroller/MainActivity.kt
+++ app/src/main/java/tech/cursor/com/diceroller/MainActivity.kt
@@ -1,15 +1,16 @@
 package tech.cursor.com.diceroller
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,11 +20,22 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
            rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt() + 1
-        resultText.text = randomInt.toString()
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when(randomInt)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 ->R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
